c3ed1280221faa095b2014a21fc11833ff3be12e
ron <ron.kirschler@gmail.com>
2014-02-24 20:27:09 +0100

    Minor changes

---
+++ src/Kir/RegExp/Builder/RegExpBuilder/SpecialCharacter/Backspace.php
@@ -0,0 +1,13 @@
+<?php
+namespace Kir\RegExp\Builder\RegExpBuilder\SpecialCharacter;
+
+use Kir\RegExp\Builder\RegExpBuilder\SpecialCharacter;
+
+class Backspace implements SpecialCharacter {
+	/**
+	 * @return string
+	 */
+	public function __toString() {
+		return '\\x5c';
+	}
+}
